c7c46e29fe96d2e4552a02ad9efa7d0f3ad213c5
Kuzdowicz, Martin (Developer) <martin.kuzdowicz@sky.uk>
2017-05-09 21:14:24 +0100

    project structure cleanup

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8888;
 
-app.use(express.static(path.resolve(__dirname, './')));
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './index.html'));
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
 });
 
 app.listen(PORT, () => {
